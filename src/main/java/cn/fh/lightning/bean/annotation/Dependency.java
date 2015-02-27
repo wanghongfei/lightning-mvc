02a3719a1b1fddf88e3ca112d46eca2931b84e91
wanghongfei <brucewhf@gmail.com>
2015-02-27 16:49:32 +0800

    create @Dependency annotation

---
+++ src/main/java/cn/fh/lightning/bean/annotation/Dependency.java
@@ -0,0 +1,15 @@
+package cn.fh.lightning.bean.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by whf on 15-2-27.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Dependency {
+    boolean required() default true;
+}
